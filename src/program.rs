0b81a67fc88e0f9ac28dbb21ab436d1167c98b07
shurizzle <shura1991@gmail.com>
2023-03-04 00:15:42 +0100

    Fix code style

--- src/program.rs
+++ src/program.rs
@@ -9,9 +9,12 @@ impl Program {
 
     pub fn eval(&self, ctx: Context, env: Environment) -> Result<Value, Error> {
         let mut last = Value::Unspecified;
-        for exp in self.0.iter() {
-            let exp = exp.clone().macroexpand(ctx.clone(), env.clone(), true)?;
-            last = exp.eval(ctx.clone(), env.clone(), true)?;
+        for exp in self.0.iter().cloned() {
+            last = exp.macroexpand(ctx.clone(), env.clone(), true)?.eval(
+                ctx.clone(),
+                env.clone(),
+                true,
+            )?;
         }
 
         Ok(last)
